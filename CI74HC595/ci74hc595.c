77e6450509f9fa1c3b26b9bce0d3a75185cbf6c3
evandro-teixeira <evandrolst@live.com>
2019-08-07 23:28:45 -0300

    criado os arquivo para o driver do CI 74HC595

---
+++ CI74HC595/ci74hc595.c
@@ -0,0 +1,10 @@
+/*
+ * ci74hc595.c
+ *
+ *  Created on: 07/08/2019
+ *      Author: Evandro Teixeira
+ */
+
+
+
+
---
+++ CI74HC595/ci74hc595.h
@@ -0,0 +1,15 @@
+/*
+ * ci74hc595.h
+ *
+ *  Created on: 07/08/2019
+ *      Author: Evandro Teixeira
+ */
+
+#ifndef THIRDPARTKL05Z_CI74HC595_CI74HC595_H_
+#define THIRDPARTKL05Z_CI74HC595_CI74HC595_H_
+
+#include "board_frdm_kl05z.h"
+
+
+
+#endif /* THIRDPARTKL05Z_CI74HC595_CI74HC595_H_ */
