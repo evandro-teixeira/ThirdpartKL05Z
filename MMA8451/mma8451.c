d7ffac2aab477f68a49fbd21b676da2f8e56d488
evandro-teixeira <evandrolst@live.com>
2019-08-19 23:16:33 -0300

    add file mma8451

---
+++ MMA8451/mma8451.c
@@ -0,0 +1,10 @@
+/*
+ * mma8451.c
+ *
+ *  Created on: 19/08/2019
+ *      Author: Evandro Teixeira
+ */
+
+
+
+
---
+++ MMA8451/mma8451.h
@@ -0,0 +1,15 @@
+/*
+ * mma8451.h
+ *
+ *  Created on: 19/08/2019
+ *      Author: Evandro Teixeira
+ */
+
+#ifndef THIRDPARTKL05Z_MMA8451_MMA8451_H_
+#define THIRDPARTKL05Z_MMA8451_MMA8451_H_
+
+
+
+
+
+#endif /* THIRDPARTKL05Z_MMA8451_MMA8451_H_ */
