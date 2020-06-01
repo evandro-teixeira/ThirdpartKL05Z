41b8f7d5e55bac8b014ff5279fb81be6d368628e
evandro-teixeira <evandrolst@live.com>
2020-06-01 14:07:40 -0300

    add arquivo para display de 7 segmentos

---
+++ DISPLAY7SEGMENT/display7segment.c
@@ -0,0 +1,10 @@
+/*
+ * display7segment.c
+ *
+ *  Created on: 01/06/2020
+ *      Author: Evandro Teixeira
+ */
+
+
+
+
---
+++ DISPLAY7SEGMENT/display7segment.h
@@ -0,0 +1,15 @@
+/*
+ * display7segment.h
+ *
+ *  Created on: 01/06/2020
+ *      Author: Evandro Teixeira
+ */
+
+#ifndef THIRDPARTKL05Z_DISPLAY7SEGMENT_DISPLAY7SEGMENT_H_
+#define THIRDPARTKL05Z_DISPLAY7SEGMENT_DISPLAY7SEGMENT_H_
+
+
+
+
+
+#endif /* THIRDPARTKL05Z_DISPLAY7SEGMENT_DISPLAY7SEGMENT_H_ */
